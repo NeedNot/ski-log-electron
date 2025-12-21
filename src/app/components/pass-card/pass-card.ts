52e61476791d4f44920cc89797b9ca6231418e3c
Need_Not <47156725+NeedNot@users.noreply.github.com>
2025-12-20 22:14:00 -0800

    color updates

--- src/app/components/pass-card/pass-card.ts
+++ src/app/components/pass-card/pass-card.ts
@@ -1,8 +1,17 @@
-import { Component, computed, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { BrnSelectImports } from '@spartan-ng/brain/select';
 import { HlmSelectImports } from '@spartan-ng/helm/select';
 import { HlmIcon } from '@spartan-ng/helm/icon';
-import { BoatSpeed, RopeLength } from '../../../types';
+import { RopeLength } from '../../../types';
 import { HlmCardImports } from '@spartan-ng/helm/card';
 import { HlmFieldImports } from '@spartan-ng/helm/field';
 import { HlmInputImports } from '@spartan-ng/helm/input';
@@ -12,8 +21,9 @@ import { NewSetStore } from '../../pages/new-set/new-set.store';
 import { BoatSpeedLabel, RopeLengthColors, RopeLengthLabel } from '../../../constants';
 import { NgClass } from '@angular/common';
 import { NgIcon, provideIcons } from '@ng-icons/core';
+import { map } from 'rxjs';
 import { lucideArrowDown, lucideArrowUp, lucidePlus, lucideTrash2 } from '@ng-icons/lucide';
-import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pass-card',
@@ -33,7 +43,7 @@ import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
   providers: [provideIcons({ lucidePlus, lucideTrash2, lucideArrowUp, lucideArrowDown })],
   templateUrl: './pass-card.html',
 })
-export class PassCard {
+export class PassCard implements OnInit {
   @Input({ required: true }) group!: FormGroup;
   @Input() index: number = 0;
   @Input() isFirst: boolean = false;
@@ -46,8 +56,20 @@ export class PassCard {
   store = inject(NewSetStore);
 
   protected readonly boatSpeeds = Object.entries(BoatSpeedLabel).reverse();
+  protected ropeColor!: WritableSignal<string>;
   protected readonly ropeLengths = Object.entries(RopeLengthLabel);
-  protected readonly ropeColor = computed(() => RopeLengthColors[RopeLength.L_0_OFF]);
+  ngOnInit(): void {
+    const control = this.group.get('ropeLength');
+    const initial = control?.value as RopeLength | undefined;
+    this.ropeColor = signal(RopeLengthColors[initial ?? RopeLength.L_15_OFF]);
+    if (control) {
+      control.valueChanges
+        .pipe(map((ropeLength) => RopeLengthColors[ropeLength as RopeLength]))
+        .subscribe((color: string) => {
+          this.ropeColor.set(color);
+        });
+    }
+  }
 
   onDragOver(event: DragEvent) {
     event.preventDefault();
