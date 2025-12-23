1e53ebc47a54650bde0c67cacec06ff736b2debd
Need_Not <47156725+NeedNot@users.noreply.github.com>
2025-12-22 16:32:52 -0800

    clean up

--- src/app/components/video-pass-card/video-pass-card.ts
+++ src/app/components/video-pass-card/video-pass-card.ts
@@ -1,10 +1,16 @@
-import { Component, computed, Input } from '@angular/core';
-import { HlmCardImports } from "@spartan-ng/helm/card";
+import { Component, Input } from '@angular/core';
+import { HlmCardImports } from '@spartan-ng/helm/card';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideArrowRight, lucideCalendar, lucideExternalLink, lucideMapPin, lucidePause, lucidePin, lucidePlay } from '@ng-icons/lucide';
-import { VideoPlayer } from "../video-player/video-player";
-import { HlmIcon } from "@spartan-ng/helm/icon";
-import { HlmBadge, HlmBadgeImports } from "@spartan-ng/helm/badge";
+import {
+  lucideArrowRight,
+  lucideCalendar,
+  lucideMapPin,
+  lucidePause,
+  lucidePlay,
+} from '@ng-icons/lucide';
+import { VideoPlayer } from '../video-player/video-player';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { HlmBadgeImports } from '@spartan-ng/helm/badge';
 import { SkiPass } from '../../../types';
 import { BoatSpeedLabel, RopeLengthColors, RopeLengthLabel } from '../../../constants';
 
@@ -12,25 +18,27 @@ import { BoatSpeedLabel, RopeLengthColors, RopeLengthLabel } from '../../../cons
   selector: 'app-video-pass-card',
   imports: [HlmCardImports, VideoPlayer, HlmIcon, NgIcon, HlmBadgeImports],
   templateUrl: './video-pass-card.html',
-  providers: [provideIcons({lucidePause, lucidePlay, lucideMapPin, lucideCalendar, lucideArrowRight})]
+  providers: [
+    provideIcons({ lucidePause, lucidePlay, lucideMapPin, lucideCalendar, lucideArrowRight }),
+  ],
 })
 export class VideoPassCard {
-  @Input() date!: string
-  @Input() pass!: SkiPass
+  @Input() date!: string;
+  @Input() pass!: SkiPass;
 
   get day() {
-    return new Date(this.date).toLocaleDateString('en-US', { month: "short", day: 'numeric' })
+    return new Date(this.date).toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
   }
 
   get speed() {
-    return BoatSpeedLabel[this.pass.speed]
+    return BoatSpeedLabel[this.pass.boatSpeed];
   }
 
   get ropeLength() {
-    return RopeLengthLabel[this.pass.ropeLength]
+    return RopeLengthLabel[this.pass.ropeLength];
   }
 
   get ropeColor() {
-    return RopeLengthColors[this.pass.ropeLength]
+    return RopeLengthColors[this.pass.ropeLength];
   }
 }
